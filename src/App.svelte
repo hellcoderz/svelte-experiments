5e3cad3008a61e967989b1e0824536940dbd537f
Vikrant Sagar <hellcoderz@pop-os.localdomain>
2020-06-06 13:22:37 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import SynTable from "./components/SynTable.svelte";
+	export let name;
+
+	let tabs = ["eng", "spa", "fra"];
+	let activeTab = "eng";
+</script>
+
+<main>
+	<SynTable {activeTab} {tabs} ></SynTable>
+</main>
+
+<style>
+	
+</style>
---
+++ src/components/SynTable.svelte
@@ -0,0 +1,40 @@
+<script>
+
+    export let activeTab;
+    export let tabs;
+
+</script>
+
+<div class="tabs">
+    <ul>
+    {#each tabs as tab}
+         <div class:active={tab === activeTab}>
+            <li>{tab}</li>
+         </div>
+    {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs {
+        margin-bottom: 40px;
+    }
+
+    ul {
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        list-style-type: none;
+    }
+
+    li {
+        margin: 0 16px;
+        
+    }
+
+    .active {
+        color: rgb(255, 102, 0);
+        border-bottom: 2px solid red;
+    }
+
+</style>
